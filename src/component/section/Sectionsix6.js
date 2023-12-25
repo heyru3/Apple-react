7d8c0ffc7566cea239cfc37564c95b1ccc34fd61
heyru3 <heyrunejmu@gmail.com>
2023-12-25 03:05:13 +0300

    sec
    sec

---
+++ src/component/section/Sectionsix6.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+function Sectionsix6() {
+  return (
+    <section className="sixth-heghlight-wrapper bg-white">
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-md-6 px-1">
+          <div className="left-side-wrapper">
+            <div className="model">16-inch model</div>
+            <div className="product-title-small">MacBook Pro</div>
+            <div className="description-wraper">The best for the brightest.</div>
+            <div className="links-wrapper">
+              <ul>
+                <li><a href="">Learn more</a></li>
+                <li><a href="">Buy</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-6 px-1">
+          <div className="right-side-wrapper">
+            <div className="top-logo-wrapper">
+              <div className="logo-wrapper">
+                <img src="images/icons/new-ipad-logo.png" />
+              </div>
+            </div>
+
+            <div className="description-wraper">
+              Like a computer. Unlike any computer.
+            </div>
+            <div className="links-wrapper">
+              <ul>
+                <li><a href="">Learn more</a></li>
+                <li><a href="">Buy</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  )
+}
+export default Sectionsix6
